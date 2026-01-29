8f3844f3c9289921fd35d4245684cf5d54bb2039
ClemNTTS <karmaclem@gmail.com>
2026-01-29 01:24:50 +0100

    testons encore

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,6 +2,9 @@ export default {
   base: "/wiki-elden-chill/",
   title: "Elden Chill Wiki",
   description: "Grimoire de l'Entre-Terre",
+  head: [
+    ["link", { rel: "icon", href: "/wiki-elden-chill/favicon.ico" }], // Correction Favicon
+  ],
   themeConfig: {
     logo: "/favicon.ico", // Si tu en as une
     nav: [
